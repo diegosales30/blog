5733a6e0982fd58c8850d352dbc654307646cc4b
diego <diegofelipesales23@gmail.com>
2022-10-13 20:56:07 -0300

    finalizando lading page com mobile first

--- src/components/LandingPage/index.tsx
+++ src/components/LandingPage/index.tsx
@@ -1,7 +1,18 @@
+import styles from "./styles.module.scss";
+
 function LandingPage() {
   return (
-    <div>
-      <h1>landing</h1>
+    <div className={styles.container}>
+      <div className={styles.containerTitle}>
+        <div className={styles.title}>
+          <h1>FaceBlog</h1>
+        </div>
+        <div className={styles.containerButton}>
+          <button>Entrar</button>
+          <button>Cadastrar</button>
+          <button>Github</button>
+        </div>
+      </div>
     </div>
   );
 }

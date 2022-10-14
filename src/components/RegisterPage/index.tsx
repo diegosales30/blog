77161952fcfacd22afc51a5304b6f15a514b48a8
diego <diegofelipesales23@gmail.com>
2022-10-14 00:14:08 -0300

    editando pagina de cadastro

--- src/components/RegisterPage/index.tsx
+++ src/components/RegisterPage/index.tsx
@@ -1,7 +1,37 @@
+import styles from "./styles.module.scss";
+
 const RegisterPage = () => {
   return (
-    <div>
-      <h1>Register</h1>
+    <div className={styles.container}>
+      <header className={styles.header}>FaceBlog</header>
+
+      <section className={styles.containerForm}>
+        <h1>SignUp</h1>
+        <form>
+          <label>
+            Username:
+            <input type="text" placeholder="ex: Jhon" />
+          </label>
+          <label>
+            Email:
+            <input type="text" placeholder="ex: jhonDoe@email.com" />
+          </label>
+          <label>
+            Password:
+            <input type="password" placeholder="password" />
+          </label>
+          <label>
+            Imagem:
+            <input type="text" placeholder="link da imagem" />
+          </label>
+        </form>
+        <div className={styles.containerButton}>
+          <button>Cadastrar</button>
+          <p>
+            Já possui conta? <span>Fazer login</span>
+          </p>
+        </div>
+      </section>
     </div>
   );
 };

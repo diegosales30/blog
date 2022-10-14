2588a5beb00f45ee5171471d6ef31a28815ca10a
diego <diegofelipesales23@gmail.com>
2022-10-14 15:55:37 -0300

    editando pagina de registro

--- src/components/RegisterPage/index.tsx
+++ src/components/RegisterPage/index.tsx
@@ -3,35 +3,36 @@ import styles from "./styles.module.scss";
 const RegisterPage = () => {
   return (
     <div className={styles.container}>
-      <header className={styles.header}>FaceBlog</header>
-
-      <section className={styles.containerForm}>
-        <h1>SignUp</h1>
-        <form>
-          <label>
-            Username:
-            <input type="text" placeholder="ex: Jhon" />
-          </label>
-          <label>
-            Email:
-            <input type="text" placeholder="ex: jhonDoe@email.com" />
-          </label>
-          <label>
-            Password:
-            <input type="password" placeholder="password" />
-          </label>
-          <label>
-            Imagem:
-            <input type="text" placeholder="link da imagem" />
-          </label>
-        </form>
-        <div className={styles.containerButton}>
-          <button>Cadastrar</button>
-          <p>
-            Já possui conta?<span>Fazer login</span>
-          </p>
-        </div>
-      </section>
+      <div className={styles.box}>
+        <h1 className={styles.title}>FaceBlog</h1>
+        <section className={styles.containerForm}>
+          <h1>SignUp</h1>
+          <form>
+            <label>
+              Username:
+              <input type="text" placeholder="ex: Jhon" />
+            </label>
+            <label>
+              Email:
+              <input type="text" placeholder="ex: jhonDoe@email.com" />
+            </label>
+            <label>
+              Password:
+              <input type="password" placeholder="password" />
+            </label>
+            <label>
+              Imagem:
+              <input type="text" placeholder="link da imagem" />
+            </label>
+          </form>
+          <div className={styles.containerButton}>
+            <button>Cadastrar</button>
+            <p>
+              Já possui conta?<span>Fazer login</span>
+            </p>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
